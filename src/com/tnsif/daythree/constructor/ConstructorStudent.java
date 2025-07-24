4495ac12fc8687791a7114d85b21d1049daf3c52
pranalichaskar7 <pranalichaskar74@gmail.com>
2025-07-24 10:50:24 +0530

    Add/UpdateConstructorstudent.java

---
+++ src/com/tnsif/daythree/constructor/ConstructorStudent.java
@@ -0,0 +1,12 @@
+package com.tnsif.daythree.constructor;
+
+public class ConstructorStudent extends Student {
+
+	public static void main(String[] args) {
+//		Student Obj = new Student();
+		
+		ConstructorStudent Obj = new ConstructorStudent();
+
+	}
+
+}
